fc096404495b21a0eac867399a04e65eb0aeb357
Sophie Gill <sophie@makersacademy.com>
2018-12-05 11:00:19 +0000

    exercise one using doubles and dependency injection

--- spec/exercise_1_spec.rb
+++ spec/exercise_1_spec.rb
@@ -1,24 +1,26 @@
 require 'exercise_1'
 
 describe Note do
-  subject(:note) { described_class.new("The Title", "This is the body")}
+  subject(:note) { described_class.new("The Title", "This is the body", note_formatter)}
+  let(:note_formatter) { double(:note_formatter) }
 
   it "has a given title" do
-    expect(subject.title).to eq "The Title"
+    expect(note.title).to eq "The Title"
   end
 
   it "has a given body" do
-    expect(subject.body).to eq "This is the body"
+    expect(note.body).to eq "This is the body"
   end
 
   it "returns a formatted note" do
-    expect(subject.display).to eq "Title: The Title\nThis is the body"
+    allow(note_formatter).to receive(:format).and_return "Title: The Title\nThis is the body"
+    expect(note.display).to eq "Title: The Title\nThis is the body"
   end
 end
 
 describe NoteFormatter do
   subject(:note_formatter) { described_class.new }
-  let(:note) { Note.new("The Title", "This is the body") }
+  let(:note) { double(:note, title: "The Title", body: "This is the body") }
   it "formats a passed in note" do
     expect(note_formatter.format(note)).to eq "Title: The Title\nThis is the body"
   end
